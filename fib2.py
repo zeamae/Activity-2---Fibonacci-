d1e99e38f4f4f0a359d9d76546d2bc247c75bf5e
zeamae <60580288+zeamae@users.noreply.github.com>
2020-02-24 19:21:09 +0800

    Update fib2.py

--- fib2.py
+++ fib2.py
@@ -1,3 +1,6 @@
+#FINAL fib2.py !!!!!!!!
+
+
 import time
 import matplotlib.pyplot as plt
 
@@ -5,181 +8,183 @@ terms = []
 run_time = []
 
 
+print("Fibonacci using Iteration")
+print("(Note: Best if not more than 40000)")
+
+i1 = int(input("No. of terms: "))
+start_time = time.time()
+terms.append(i1)
 
 def iterfib1():
 
-    print("Range from 0 to 40000")
-    nterms = int(input("No. of terms: "))
-    start_time = time.time()
-    terms.append(nterms)
 
 
     n1, n2 = 0, 1
     count = 0
 
-    if nterms <= 0:
+    if i1 <= 0:
         print("Please enter a positive integer")
-    elif nterms == 1:
-        print("Fibonacci sequence upto", nterms, ":")
+    elif i1 == 1:
+        print("Fibonacci sequence upto", i1, ":")
         print(n1)
     else:
         print("Fibonacci sequence:")
-        while count < nterms:
+        while count < i1:
             print(n1)
             nth = n1 + n2
 
             n1 = n2
             n2 = nth
             count += 1
-
-    end_time = time.time()
-    runTime = end_time - start_time
-    print("Execution time:", runTime, "seconds")
-    run_time.append(runTime)
 
 iterfib1()
 
+end_time = time.time()
+runTime1 = end_time - start_time
+print("Execution time:", runTime1)
+
+
+print("----------------------------------------------")
+print("(Note: Best if not more than 40000)")
+i2 = int(input("No. of terms: "))
+start_time = time.time()
+terms.append(i2)
 
 def iterfib2():
 
-    print("----------------------------------------------")
-    print("Range from 0 to 40000")
-    nterms = int(input("No. of terms: "))
-    start_time = time.time()
-    terms.append(nterms)
-
-
     n1, n2 = 0, 1
     count = 0
 
-    if nterms <= 0:
+    if i2 <= 0:
         print("Please enter a positive integer")
-    elif nterms == 1:
-        print("Fibonacci sequence upto", nterms, ":")
+    elif i2 == 1:
+        print("Fibonacci sequence upto", i2, ":")
         print(n1)
     else:
         print("Fibonacci sequence:")
-        while count < nterms:
+        while count < i2:
             print(n1)
             nth = n1 + n2
 
             n1 = n2
             n2 = nth
             count += 1
-
-    end_time = time.time()
-    runTime = end_time - start_time
-    print("Execution time:", runTime, "seconds")
-    run_time.append(runTime)
-
 iterfib2()
 
+end_time = time.time()
+runTime2 = end_time - start_time
+print("Execution time:", runTime2)
+
+
+
+print("----------------------------------------------")
+print("(Note: Best if not more than 40000)")
+i3 = int(input("No. of terms: "))
+start_time = time.time()
+terms.append(i3)
 
 def iterfib3():
-    print("----------------------------------------------")
-    print("Range from 0 to 40000")
-    nterms = int(input("No. of terms: "))
-    start_time = time.time()
-    terms.append(nterms)
-
-
     n1, n2 = 0, 1
     count = 0
 
-    if nterms <= 0:
+    if i3 <= 0:
         print("Please enter a positive integer")
-    elif nterms == 1:
-        print("Fibonacci sequence upto", nterms, ":")
+    elif i3 == 1:
+        print("Fibonacci sequence upto", i3, ":")
         print(n1)
     else:
         print("Fibonacci sequence:")
-        while count < nterms:
+        while count < i3:
             print(n1)
             nth = n1 + n2
 
             n1 = n2
             n2 = nth
             count += 1
-
-    end_time = time.time()
-    runTime = end_time - start_time
-    print("Execution time:", runTime, "seconds")
-    run_time.append(runTime)
 
 iterfib3()
 
+end_time = time.time()
+runTime3 = end_time - start_time
+print("Execution time:", runTime3)
+
+
+print("----------------------------------------------")
+print("(Note: Best if not more than 40000)")
+i4 = int(input("No. of terms: "))
+start_time = time.time()
+terms.append(i4)
 
 def iterfib4():
-    print("----------------------------------------------")
-    print("Range from 0 to 40000")
-    nterms = int(input("No. of terms: "))
-    start_time = time.time()
-    terms.append(nterms)
-
-
     n1, n2 = 0, 1
     count = 0
 
-    if nterms <= 0:
+    if i4 <= 0:
         print("Please enter a positive integer")
-    elif nterms == 1:
-        print("Fibonacci sequence upto", nterms, ":")
+    elif i4 == 1:
+        print("Fibonacci sequence upto", i4, ":")
         print(n1)
     else:
         print("Fibonacci sequence:")
-        while count < nterms:
+        while count < i4:
             print(n1)
             nth = n1 + n2
 
             n1 = n2
             n2 = nth
             count += 1
-
-    end_time = time.time()
-    runTime = end_time - start_time
-    print("Execution time:", runTime, "seconds")
-    run_time.append(runTime)
 
 iterfib4()
 
+end_time = time.time()
+runTime4 = end_time - start_time
+print("Execution time:", runTime4)
+
+
+
+print("----------------------------------------------")
+print("(Note: Best if not more than 40000)")
+i5 = int(input("No. of terms: "))
+start_time = time.time()
+terms.append(i5)
 
 def iterfib5():
-    print("----------------------------------------------")
-    print("Range from 0 to 40000")
-    nterms = int(input("No. of terms: "))
-    start_time = time.time()
-    terms.append(nterms)
-
-
     n1, n2 = 0, 1
     count = 0
 
-    if nterms <= 0:
+    if i5 <= 0:
         print("Please enter a positive integer")
-    elif nterms == 1:
-        print("Fibonacci sequence upto", nterms, ":")
+    elif i5 == 1:
+        print("Fibonacci sequence upto", i5, ":")
         print(n1)
     else:
         print("Fibonacci sequence:")
-        while count < nterms:
+        while count < i5:
             print(n1)
             nth = n1 + n2
 
             n1 = n2
             n2 = nth
             count += 1
-
-    end_time = time.time()
-    runTime = end_time - start_time
-    print("Execution time:", runTime, "seconds")
-    run_time.append(runTime)
-
 iterfib5()
 
+end_time = time.time()
+runTime5 = end_time - start_time
+print("Execution time:", runTime5)
 
-plt.plot(terms,run_time, 'bo')
-plt.xlabel('Integers (Positive)')
-plt.ylabel('Execution Time')
-plt.axis([0, 40000, 0, 20])
-plt.title("Fibonacci Graph")
+
+
+
+left = [1, 2, 3, 4, 5]
+height = [runTime1, runTime2, runTime3, runTime4, runTime5]
+
+tick_label = [i1, i2, i3, i4, i5]
+
+plt.bar(left, height, tick_label=tick_label,
+        width=0.5, color=['red'])
+
+plt.xlabel('Fibonacci Numbers Generated')
+plt.ylabel('Execution Time (seconds)')
+plt.title('Fibonacci Runtime (Iteration)')
+
 plt.show()
